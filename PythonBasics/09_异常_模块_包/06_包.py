9320de903a2291e64cbf8eeca33df4226650cb9c
Runze Yang <ryanyyyyyyy@163.com>
2024-03-07 20:31:05 +1100

    Update on the comments of package import content chapter

--- PythonBasics/09_异常_模块_包/06_包.py
+++ PythonBasics/09_异常_模块_包/06_包.py
@@ -4,11 +4,11 @@
 
 # 创建一个包
 # 导入自定义的包中的模块，并使用
-# import my_package.my_module1
-# import my_package.my_module2
+import PythonBasics.my_package.my_module1
+import PythonBasics.my_package.my_module2
 #
-# my_package.my_module1.info_print1()
-# my_package.my_module2.info_print2()
+PythonBasics.my_package.my_module1.info_print1()
+PythonBasics.my_package.my_module2.info_print2()
 
 # from my_package import my_module1
 # from my_package import my_module2
@@ -21,6 +21,6 @@
 # info_print2()
 
 # 通过__all__变量，控制import *
-from PythonBasics.my_package import *
-my_module1.info_print1()
-my_module2.info_print2()
+# from PythonBasics.my_package import *
+# my_module1.info_print1()
+# my_module2.info_print2()
