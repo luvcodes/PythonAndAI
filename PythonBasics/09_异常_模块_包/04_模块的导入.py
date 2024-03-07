3b4d0eb4193e6e29c3fd35d70b5aad1cfcd98fb4
Runze Yang <ryanyyyyyyy@163.com>
2024-03-08 00:35:51 +1100

    Update on the package import

--- PythonBasics/09_异常_模块_包/04_模块的导入.py
+++ PythonBasics/09_异常_模块_包/04_模块的导入.py
@@ -5,9 +5,11 @@
 from time import sleep
 
 # 使用import导入time模块使用sleep功能（函数）
-# import time     # 导入Python内置的time模块（time.py这个代码文件）
+# import time
+# 导入Python内置的time模块（time.py这个代码文件）
 # print("你好")
-# time.sleep(5)   # 通过. 就可以使用模块内部的全部功能（类、函数、变量）
+# 通过. 就可以使用模块内部的全部功能（类、函数、变量）
+# time.sleep(5)
 # print("我好")
 
 # 使用from导入time的sleep功能（函数）
