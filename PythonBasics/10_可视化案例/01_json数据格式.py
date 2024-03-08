626138e21817c7873cfbb8b63e0c871cc0a30883
Runze Yang <ryanyyyyyyy@163.com>
2024-03-08 15:44:49 +1100

    JSON data type content learning

--- PythonBasics/10_可视化案例/01_json数据格式.py
+++ PythonBasics/10_可视化案例/01_json数据格式.py
@@ -8,20 +8,23 @@ data = [{"name": "张大山", "age": 11}, {"name": "王大锤", "age": 13}, {"na
 json_str = json.dumps(data, ensure_ascii=False)
 print(type(json_str))
 print(json_str)
+print("\n")
 
 # 准备字典，将字典转换为JSON
 d = {"name":"周杰轮", "addr":"台北"}
 json_str = json.dumps(d, ensure_ascii=False)
 print(type(json_str))
 print(json_str)
+print("\n")
 
-# 将JSON字符串转换为Python数据类型[{k: v, k: v}, {k: v, k: v}]
+# 将JSON字符串转换为Python列表数据类型[{k: v, k: v}, {k: v, k: v}]
 s = '[{"name": "张大山", "age": 11}, {"name": "王大锤", "age": 13}, {"name": "赵小虎", "age": 16}]'
 l = json.loads(s)
 print(type(l))
 print(l)
+print("\n")
 
-# 将JSON字符串转换为Python数据类型{k: v, k: v}
+# 将JSON字符串转换为Python字典数据类型{k: v, k: v}
 s = '{"name": "周杰轮", "addr": "台北"}'
 d = json.loads(s)
 print(type(d))
