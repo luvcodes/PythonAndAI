799f2d7a64268a9bc46a78d180d1490e4e7ce549
Runze Yang <ryanyyyyyyy@163.com>
2024-05-19 20:16:35 +1000

    Update the folder structure and notes

--- PythonBasics/02_Python入门语法/07_Python的字符串格式化.py
+++ PythonBasics/02_Python入门语法/07_Python的字符串格式化.py
@@ -1,23 +1,35 @@
-# 通过占位的形式，完成拼接
-name = "黑马程序员"
-message = "学IT来：%s" % name
-print(message)
+# 通过%的方式，格式化输出
+# 第一种: 通过%s的方式，格式化string类型
+name = "Python"
+print("Hello %s" % name)
+# 第二种: 通过%d的方式，格式化int类型
+age = 23
+print("I'm %d years old" % age)
+age2 = 23.2323
+print("I'm %.2f years old" % age2)
+# 第三种：通过%f的方式，格式化float类型
+currentTime = 16.4603
+print("Current time is %f " % currentTime)
+print("宽度限制：%5d" % 10)  # 输出：宽度限制：   10
+print("小数精度：%7.2f" % 3.14159)  # 输出：小数精度：   3.14
+print("左对齐：%-5d" % 10)  # 输出：左对齐：10
+print("-------------------------")
 
-# 通过占位的形式，完成数字和字符串的拼接
-class_num = 57
-avg_salary = 16781
-message = "Python大数据学科，北京%s期，毕业平均工资：%s" % (class_num, avg_salary)
-print(message)
+# 使用str.format()方式来格式化字符串
+print("Hello, {}!".format("Alice"))  # 输出：Hello, Alice!
+# print("整数：{0}, 浮点数：{1:.2f}".format(10, 3.14159))  # 输出：整数：10, 浮点数：3.14
+print("浮点数: {1:.1f}, 整数: {0}".format(10, 3.14159))  # 输出：浮点数: 3.1, 整数: 10
+print("姓名：{name}, 年龄：{age}".format(name="Alice", age=25))  # 输出：姓名：Alice, 年龄：25
+print("左对齐：{:<5}".format(10))  # 输出：左对齐：10
+print("小数精度：{:.2f}".format(3.14159))  # 输出：小数精度：3.14
+print("宽度和精度：{:<7.2f}".format(3.14159))  # 输出：宽度和精度：3.14
+print("-------------------------")
 
-name = "传智播客"
-setup_year = 2006
-stock_price = 19.99
-message = "%s，成立于：%d，我今天的股价是：%f" % (name, setup_year, stock_price)
-print(message)
-
-num1 = 11
-num2 = 11.345
-print("数字11宽度限制5，结果是：%5d" % num1)
-print("数字11宽度限制1，结果是：%1d" % num1)
-print("数字11.345宽度限制7，小数精度2，结果是：%7.2f" % num2)
-print("数字11.345不限制，小数精度2，结果是：%.2f" % num2)
+# 使用f-string的方式来格式化字符串
+name = "Alice"
+age = 25
+print(f"姓名：{name}, 年龄：{age}")  # 输出：姓名：Alice, 年龄：25
+num = 3.14159
+print(f"宽度限制：{num:5}")  # 输出：宽度限制：3.14159
+print(f"小数精度：{num:.2f}")  # 输出：小数精度：3.14
+print(f"宽度和精度：{num:<7.2f}")  # 输出：宽度和精度：3.14
---
+++ PythonBasics/02_Python入门语法/08_Python的字符串相关方法.py
@@ -0,0 +1,19 @@
+# 首字母大写
+name = "i love python"
+print(name.title())
+
+# 全大写以及全小写
+name_2 = "Ada"
+print(name_2.upper())
+print(name_2.lower())
+
+# 删除字符串中的空白
+favorite_language = ' Python '
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
+
+# 删除前缀
+nostarch_url = 'https://nostarch.com'
+print(nostarch_url.removeprefix("https://"))
+
