a0fac0773262c85702e6db97c63284c15332b685
Runze Yang <ryanyyyyyyy@163.com>
2024-05-20 22:00:49 +1000

    Update on the loops in Python

--- PythonBasics/04_Python循环语句/05_2_for循环练习题-数一数有几个a.py
+++ PythonBasics/04_Python循环语句/05_2_for循环练习题-数一数有几个a.py
@@ -3,12 +3,7 @@
 """
 # 统计如下字符串中，有多少个字母a
 name = "itheima is a brand of itcast"
-
-# 定义一个变量，用来统计有多少个a
 count = 0
-
-# for 循环统计
-# for 临时变量 in 被统计的数据:
 for x in name:
     if x == "a":
         count += 1
--- PythonBasics/04_Python循环语句/05_for循环的基础语法.py
+++ PythonBasics/04_Python循环语句/05_for循环的基础语法.py
@@ -8,3 +8,7 @@ for x in name:
     # 将name的内容，挨个取出赋予x临时变量
     # 就可以在循环体内对x进行处理
     print(x)
+
+nameList = ['itheima', 'python', 'Java']
+for name in nameList:
+    print(f"Name: {name.title()}, that was a good name!")
